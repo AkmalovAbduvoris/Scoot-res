78d15721ada910452f503df9ac2edcd84081692e
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-05-04 23:50:31 +0500

    Voris

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <script setup>
 import Easy from '../components/Home/Easy/Easy.vue';
-import Sign from '../components/Home/Hero/Sign/Sign.vue';
+import Sign from '../components/Home/Sign/Sign.vue'
 import Hero from '../components/Home/Hero/hero.vue';
 import Locate from '../components/Home/Locate/Locate.vue';
 
