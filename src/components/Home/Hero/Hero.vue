89fb29a27aad500857bccc319cc801cbd0ff8bcc
AkmalovAbduvoris <akmalovabduvoris@gmail.com>
2023-07-05 10:13:27 +0500

    aaaa

--- src/components/Home/Hero/Hero.vue
+++ src/components/Home/Hero/Hero.vue
@@ -10,9 +10,10 @@ import './hero.scss'
                     <div class="hero__card-wrapper">
                         <p class="hero__text">Scoot takes the hassle out of urban mobility. Our bikes are placed in convenient locations in each of our cities. Use our app to locate the nearest bike, unlock it with a tap, and you’re away!</p>
                         <button class="hero__btn">Get Scootin</button>  
-                    </div>  
+                    </div>
                 </div>
             </div>
         </div>
+        
     </section>
 </template>
